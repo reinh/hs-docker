4d1c923eba4dc656a2444f6eecf87263e1483888
Rein Henrichs <rhenrichs@pivotmail.com>
2014-10-13 15:16:15 -0700

    Add createContainer

--- src/Network/Docker/Client/Containers.hs
+++ src/Network/Docker/Client/Containers.hs
@@ -6,16 +6,19 @@
 module Network.Docker.Client.Containers where
 
 import           Control.Applicative
-import           Control.Lens
+import           Control.Lens                   hiding ((.=))
 import           Control.Lens.TH
 import           Data.Aeson
+import           Data.Aeson.Types
 import           Data.ByteString.Lazy           (ByteString)
+import           Data.Default
 import           Data.List.Split
 import           Data.Text                      (Text)
 import qualified Data.Text                      as T
 import           Data.Thyme
 import           Network.Docker.Client.Core
 import qualified Network.Wreq                   as W
+import           Network.Wreq.Types             (Postable)
 import           Prelude                        hiding (all)
 import           Text.PrettyPrint
 import           Text.PrettyPrint.GenericPretty
@@ -47,8 +50,80 @@ instance FromJSON Container where
               <*> o .: "Names"
               <*> o .: "Status"
 
-containers :: Client -> IO (W.Response [Container])
-containers = containersWith W.defaults
+listContainers :: Client -> IO (W.Response [Container])
+listContainers = listContainersWith W.defaults
 
-containersWith :: W.Options -> Client ->  IO (W.Response [Container])
-containersWith opts c  = W.asJSON =<< getWith opts c "containers/json"
+listContainersWith :: W.Options -> Client ->  IO (W.Response [Container])
+listContainersWith opts c  = W.asJSON =<< getWith opts c "containers/json"
+
+data CreateRequest =
+  CreateRequest {_createHostname        :: String
+                ,_createDomainname      :: String
+                ,_createUser            :: String
+                ,_createMemory          :: Int
+                ,_createMemorySwap      :: Int
+                ,_createCpuShares       :: Int
+                ,_createCpuset          :: String
+                ,_createAttachStdin     :: Bool
+                ,_createAttachStdout    :: Bool
+                ,_createAttachStderr    :: Bool
+                ,_createPortSpecs       :: Maybe String
+                ,_createExposedPorts    :: [(String,String)]
+                ,_createTty             :: Bool
+                ,_createOpenStdin       :: Bool
+                ,_createStdinOnce       :: Bool
+                ,_createEnv             :: [String]
+                ,_createCmd             :: [String]
+                ,_createImage           :: String
+                ,_createVolumes         :: [(String,String)]
+                ,_createWorkingDir      :: String
+                ,_createEntrypoint      :: Maybe String
+                ,_createNetworkDisabled :: Bool
+                ,_createOnBuild         :: Maybe String}
+  deriving (Show,Generic)
+
+makeLenses ''CreateRequest
+
+instance Default CreateRequest where
+  def =
+    CreateRequest {_createHostname        = ""
+                  ,_createDomainname      = ""
+                  ,_createUser            = ""
+                  ,_createMemory          = 0
+                  ,_createMemorySwap      = 0
+                  ,_createCpuShares       = 0
+                  ,_createCpuset          = ""
+                  ,_createAttachStdin     = False
+                  ,_createAttachStdout    = False
+                  ,_createAttachStderr    = False
+                  ,_createPortSpecs       = Nothing
+                  ,_createExposedPorts    = []
+                  ,_createTty             = False
+                  ,_createOpenStdin       = False
+                  ,_createStdinOnce       = False
+                  ,_createEnv             = []
+                  ,_createCmd             = []
+                  ,_createImage           = ""
+                  ,_createVolumes         = []
+                  ,_createWorkingDir      = ""
+                  ,_createEntrypoint      = Nothing
+                  ,_createNetworkDisabled = False
+                  ,_createOnBuild         = Nothing}
+
+
+instance ToJSON CreateRequest where
+  toJSON = genericToJSON (defaultOptions {fieldLabelModifier = drop 7
+                                         ,omitNothingFields = False})
+
+data CreateResponse =
+  CreateResponse {_createResponseId       :: String
+                 ,_createResponseWarnings :: Maybe [String]}
+  deriving (Show,Generic)
+
+makeLenses ''CreateResponse
+
+instance FromJSON CreateResponse where
+  parseJSON = genericParseJSON (defaultOptions {fieldLabelModifier = drop 15})
+
+createContainer :: Client -> W.Options -> CreateRequest -> IO (W.Response CreateResponse)
+createContainer c opts payload = W.asJSON =<< postWith opts c "containers/create" (toJSON payload)
--- src/Network/Docker/Client/Core.hs
+++ src/Network/Docker/Client/Core.hs
@@ -4,12 +4,14 @@
 module Network.Docker.Client.Core where
 
 import           Control.Lens
+import           Data.Aeson
 import           Data.ByteString.Lazy           (ByteString)
 import           Data.Text                      (Text)
 import qualified Data.Text                      as T
 import           Data.Thyme
 import           Data.Thyme.Clock.POSIX
 import qualified Network.Wreq                   as W
+import           Network.Wreq.Types             (Postable)
 import           Text.PrettyPrint
 import           Text.PrettyPrint.GenericPretty
 
@@ -21,6 +23,12 @@ getWith opts (Client url) path = W.getWith opts (url </> path)
 
 get :: Client -> String -> IO (W.Response ByteString)
 get c path = getWith W.defaults c path
+
+postWith :: Postable a => W.Options -> Client -> String -> a -> IO (W.Response ByteString)
+postWith opts (Client url) path p = W.postWith opts (url </> path) p
+
+post :: Postable a => Client -> String -> a -> IO (W.Response ByteString)
+post (Client url) path p = W.postWith W.defaults (url </> path) p
 
 (</>) :: String -> String -> String
 (</>) xs ys = xs ++ "/" ++ ys
